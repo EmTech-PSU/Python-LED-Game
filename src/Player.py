4bb81faf46a1f58b0551746c5c243afb6fa915bb
Collin Kleest <collinkleest@gmail.com>
2019-11-18 09:14:02 -0500

    average

--- src/Player.py
+++ src/Player.py
@@ -1,11 +1,17 @@
 
-
-
 class Player:
-    pass
 
-p1 = Player()
-p2 = Player()
+    #Constructor includes name, hits, misses, and the average
+    def __init__(self, playerName, hits, misses, average):
+        self.playerName = playerName
+        self.hits = hits
+        self.misses = misses
+        self.average = getAverage()
 
-print(p1)
-print(p2)
+    # method for hit & miss percentiale
+    def getAverage(self):
+        total = self.hits + self.misses
+        averageHitPercent  = (total / self.hits)*100
+        averageMissPercent = (total / self.misses)*100
+        return averageHitPercent, averageMissPercent
+        
